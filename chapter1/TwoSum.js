ed5fffdecca0450121ffd46bd3c2aab029e04816
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-03-18 09:11:43 -0400

    TwoSum

--- chapter1/TwoSum.js
+++ chapter1/TwoSum.js
@@ -1,7 +1,7 @@
 function sumZero(arr){
     let left =0;
     let right=0;
-    while (let<right){
+    while (left<right){
         let sum=arr[left]+arr[right];
         if(sum==0){
             return [arr[left],arr[right]];
