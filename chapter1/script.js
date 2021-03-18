079768ff0fc924b3f5f9ecac8172786155b61771
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-03-17 20:33:27 -0400

    change the chapter1

--- chapter1/script.js
+++ chapter1/script.js
@@ -31,9 +31,10 @@ var checkPermute = function(stringOne, stringTwo) {
     }
   };
   
-  // Tests
+  
   console.log(checkPermute('aba', 'aab'), true);
   
   console.log(checkPermute('aba', 'aaba'), false);
   
+  console.log(checkPermute('aba', 'aa'), false);
   console.log(checkPermute('aba', 'aa'), false);
